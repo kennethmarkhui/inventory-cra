de50934899c70f0323a6b57e1de6b91568c41204
Kenneth Mark Hui <kennethmarkhui@gmail.com>
2020-05-12 19:19:41 +0800

    Redirect EditItem to / if props.location.data is empty -happens if user enter url to edit item instead of the edit button

--- src/components/EditItem.js
+++ src/components/EditItem.js
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Redirect } from 'react-router-dom';
+import Spinner from './Spinner';
 
 const EditItem = (props) => {
   // console.log(props.location.data);
+  const [redirct, setRedirct] = useState(false);
 
-  return (
-    <div>
-      <h1>ID:{props.location.data.id}</h1>
-      <h1>TITLE:{props.location.data.title}</h1>
-    </div>
-  );
+  useEffect(() => {
+    if (!props.location.data) {
+      setRedirct(true);
+    }
+  }, [props]);
+
+  if (redirct) {
+    return <Redirect to="/" />;
+  } else {
+    let content = <Spinner />;
+    if (props.location.data) {
+      content = (
+        <div>
+          <h1>ID:{props.location.data.id}</h1>
+          <h1>TITLE:{props.location.data.title}</h1>
+        </div>
+      );
+    }
+    return content;
+  }
 };
 
 export default EditItem;
