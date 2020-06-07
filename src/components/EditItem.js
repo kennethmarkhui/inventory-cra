7ba70c4238880d615f72dceb8acc195eb0d54127
Kenneth Mark Hui <kennethmarkhui@gmail.com>
2020-06-07 21:49:01 +0800

    removed spinner for EditItem when fetchItem is loading as it interfere with the form data when error occured.

--- src/components/EditItem.js
+++ src/components/EditItem.js
@@ -19,8 +19,6 @@ import {
 import { useForm, useFieldArray } from 'react-hook-form';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-import Spinner from './Spinner';
-
 import ItemsContext from '../context/items/itemsContext';
 
 let renderCount = 0;
@@ -99,194 +97,193 @@ const EditItem = (props) => {
 
   return (
     <React.Fragment>
-      {isLoading && <Spinner />}
-      {!isLoading && item && (
-        <React.Fragment>
-          <Card>
-            <CardHeader className="h6">Edit Item</CardHeader>
-            <CardBody>
-              <Form onSubmit={handleSubmit(onFormUpdateSubmit)}>
-                <FormGroup row>
-                  <Label md="3" htmlFor="category">
-                    Category
-                  </Label>
-                  <Col md="9">
-                    <Input
-                      type="select"
-                      id="category"
-                      name="category"
-                      innerRef={register}
-                    >
-                      <option>Display Art</option>
-                      <option>Scroll</option>
-                      <option>Others</option>
-                    </Input>
-                  </Col>
-                </FormGroup>
+      <React.Fragment>
+        <Card>
+          <CardHeader className="h6">Edit Item</CardHeader>
+          <CardBody>
+            <Form onSubmit={handleSubmit(onFormUpdateSubmit)}>
+              <FormGroup row>
+                <Label md="3" htmlFor="category">
+                  Category
+                </Label>
+                <Col md="9">
+                  <Input
+                    type="select"
+                    id="category"
+                    name="category"
+                    innerRef={register}
+                  >
+                    <option>Display Art</option>
+                    <option>Scroll</option>
+                    <option>Others</option>
+                  </Input>
+                </Col>
+              </FormGroup>
 
-                <hr />
+              <hr />
 
-                <Input
-                  type="hidden"
-                  id="_id"
-                  name="_id"
-                  innerRef={register}
-                  readOnly
-                />
+              <Input
+                type="hidden"
+                id="_id"
+                name="_id"
+                innerRef={register}
+                readOnly
+              />
 
-                <Input
-                  type="hidden"
-                  id="prevRefId"
-                  name="prevRefId"
-                  innerRef={register}
-                  readOnly
-                />
+              <Input
+                type="hidden"
+                id="prevRefId"
+                name="prevRefId"
+                innerRef={register}
+                readOnly
+              />
 
-                <FormGroup row>
-                  <Label lg="3" htmlFor="refId">
-                    Reference ID
-                  </Label>
-                  <Col lg="9">
-                    <Input
-                      id="refId"
-                      name="refId"
-                      innerRef={register({
-                        required: 'Reference ID is required',
-                      })}
-                      invalid={!!errors.refId}
-                    />
-                    {errors.refId && errors.refId.type === 'required' && (
-                      <FormFeedback>{errors.refId.message}</FormFeedback>
-                    )}
-                  </Col>
-                </FormGroup>
+              <FormGroup row>
+                <Label lg="3" htmlFor="refId">
+                  Reference ID
+                </Label>
+                <Col lg="9">
+                  <Input
+                    id="refId"
+                    name="refId"
+                    innerRef={register({
+                      required: 'Reference ID is required',
+                    })}
+                    invalid={!!errors.refId}
+                  />
+                  {errors.refId && errors.refId.type === 'required' && (
+                    <FormFeedback>{errors.refId.message}</FormFeedback>
+                  )}
+                </Col>
+              </FormGroup>
 
-                <FormGroup row>
-                  <Label lg="3" htmlFor="name">
-                    Name
-                  </Label>
-                  <Col lg="9">
-                    <Input
-                      id="name"
-                      name="name"
-                      innerRef={register({ required: 'Name is required' })}
-                      invalid={!!errors.name}
-                    />
-                    {errors.name && errors.name.type === 'required' && (
-                      <FormFeedback>{errors.name.message}</FormFeedback>
-                    )}
-                  </Col>
-                </FormGroup>
+              <FormGroup row>
+                <Label lg="3" htmlFor="name">
+                  Name
+                </Label>
+                <Col lg="9">
+                  <Input
+                    id="name"
+                    name="name"
+                    innerRef={register({ required: 'Name is required' })}
+                    invalid={!!errors.name}
+                  />
+                  {errors.name && errors.name.type === 'required' && (
+                    <FormFeedback>{errors.name.message}</FormFeedback>
+                  )}
+                </Col>
+              </FormGroup>
 
-                <FormGroup row>
-                  <Label lg="3" htmlFor="storage">
-                    Storage
-                  </Label>
-                  <Col lg="9">
-                    <Input
-                      id="storage"
-                      name="storage"
-                      innerRef={register({ required: 'Storage is required' })}
-                      invalid={!!errors.storage}
-                    />
-                    {errors.storage && errors.storage.type === 'required' && (
-                      <FormFeedback>{errors.storage.message}</FormFeedback>
-                    )}
-                  </Col>
-                </FormGroup>
+              <FormGroup row>
+                <Label lg="3" htmlFor="storage">
+                  Storage
+                </Label>
+                <Col lg="9">
+                  <Input
+                    id="storage"
+                    name="storage"
+                    innerRef={register({ required: 'Storage is required' })}
+                    invalid={!!errors.storage}
+                  />
+                  {errors.storage && errors.storage.type === 'required' && (
+                    <FormFeedback>{errors.storage.message}</FormFeedback>
+                  )}
+                </Col>
+              </FormGroup>
 
-                <FormGroup row>
-                  <Label lg="3" htmlFor="country">
-                    Country
-                  </Label>
-                  <Col lg="9">
-                    <Input
-                      id="country"
-                      name="location.country"
-                      innerRef={register({ required: 'Country is required' })}
-                      invalid={!!errors.location && !!errors.location.country}
-                    />
-                    {errors.location && errors.location.country && (
-                      <FormFeedback>
-                        {errors.location.country.message}
-                      </FormFeedback>
-                    )}
-                  </Col>
-                </FormGroup>
+              <FormGroup row>
+                <Label lg="3" htmlFor="country">
+                  Country
+                </Label>
+                <Col lg="9">
+                  <Input
+                    id="country"
+                    name="location.country"
+                    innerRef={register({ required: 'Country is required' })}
+                    invalid={!!errors.location && !!errors.location.country}
+                  />
+                  {errors.location && errors.location.country && (
+                    <FormFeedback>
+                      {errors.location.country.message}
+                    </FormFeedback>
+                  )}
+                </Col>
+              </FormGroup>
 
-                <FormGroup row>
-                  <Label lg="3" htmlFor="area">
-                    Area
-                  </Label>
-                  <Col lg="9">
-                    <Input id="area" name="location.area" innerRef={register} />
-                  </Col>
-                </FormGroup>
+              <FormGroup row>
+                <Label lg="3" htmlFor="area">
+                  Area
+                </Label>
+                <Col lg="9">
+                  <Input id="area" name="location.area" innerRef={register} />
+                </Col>
+              </FormGroup>
 
-                <FormGroup row>
-                  <Label lg="3" htmlFor="period">
-                    Period
-                  </Label>
-                  <Col lg="9">
-                    <Input id="period" name="period" innerRef={register} />
-                  </Col>
-                </FormGroup>
+              <FormGroup row>
+                <Label lg="3" htmlFor="period">
+                  Period
+                </Label>
+                <Col lg="9">
+                  <Input id="period" name="period" innerRef={register} />
+                </Col>
+              </FormGroup>
 
-                <FormGroup row>
-                  <Label lg="3" htmlFor="sizes">
-                    Sizes <Badge>{fields.length}</Badge>
-                  </Label>
-                  <Col lg="9">
-                    <Row>
-                      {fields.map((size, index) => (
-                        <Col sm="6" key={size.id}>
-                          <InputGroup>
-                            <Input
-                              type="number"
-                              min="0"
-                              step=".25"
-                              name={`sizes[${index}].len`}
-                              defaultValue={size.len}
-                              innerRef={register()}
+              <FormGroup row>
+                <Label lg="3" htmlFor="sizes">
+                  Sizes <Badge>{fields.length}</Badge>
+                </Label>
+                <Col lg="9">
+                  <Row>
+                    {fields.map((size, index) => (
+                      <Col sm="6" key={size.id}>
+                        <InputGroup>
+                          <Input
+                            type="number"
+                            min="0"
+                            step=".25"
+                            name={`sizes[${index}].len`}
+                            defaultValue={size.len}
+                            innerRef={register()}
+                          />
+                          <InputGroupAddon addonType="append">
+                            <Button
+                              close
+                              outline
+                              onClick={() => {
+                                remove(index);
+                              }}
                             />
-                            <InputGroupAddon addonType="append">
-                              <Button
-                                close
-                                outline
-                                onClick={() => {
-                                  remove(index);
-                                }}
-                              />
-                            </InputGroupAddon>
-                            <Input
-                              type="number"
-                              min="0"
-                              step=".25"
-                              name={`sizes[${index}].wid`}
-                              defaultValue={size.wid}
-                              innerRef={register()}
-                            />
-                          </InputGroup>
-                        </Col>
-                      ))}
-                      {fields.length < 2 && (
-                        <Col sm="6" className="text-center">
-                          <Button
-                            outline
-                            onClick={() => {
-                              append({ len: '', wid: '' });
-                            }}
-                          >
-                            <FontAwesomeIcon icon="plus" />
-                          </Button>
-                        </Col>
-                      )}
-                    </Row>
-                  </Col>
-                </FormGroup>
+                          </InputGroupAddon>
+                          <Input
+                            type="number"
+                            min="0"
+                            step=".25"
+                            name={`sizes[${index}].wid`}
+                            defaultValue={size.wid}
+                            innerRef={register()}
+                          />
+                        </InputGroup>
+                      </Col>
+                    ))}
+                    {fields.length < 2 && (
+                      <Col sm="6" className="text-center">
+                        <Button
+                          outline
+                          onClick={() => {
+                            append({ len: '', wid: '' });
+                          }}
+                        >
+                          <FontAwesomeIcon icon="plus" />
+                        </Button>
+                      </Col>
+                    )}
+                  </Row>
+                </Col>
+              </FormGroup>
 
-                <hr />
+              <hr />
 
+              <FormGroup>
                 <Button className="float-right" disabled={isLoading}>
                   {!isLoading ? (
                     'Confirm'
@@ -305,13 +302,13 @@ const EditItem = (props) => {
                 >
                   Cancel
                 </Button>
-              </Form>
-            </CardBody>
-            <p>watch(): {JSON.stringify(watch({ nest: true }))}</p>
-            Test Component Render Count: {renderCount}
-          </Card>
-        </React.Fragment>
-      )}
+              </FormGroup>
+            </Form>
+          </CardBody>
+          <p>watch(): {JSON.stringify(watch({ nest: true }))}</p>
+          Test Component Render Count: {renderCount}
+        </Card>
+      </React.Fragment>
     </React.Fragment>
   );
 };
